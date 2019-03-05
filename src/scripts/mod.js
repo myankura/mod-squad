209a8ac3beed3710c018de9b201fbc4cdc233883
Michael Yankura <myankura210@gmail.com>
2019-03-05 13:33:10 -0600

    push completed project to github.

---
+++ src/scripts/mod.js
@@ -0,0 +1,20 @@
+console.log("mod.js")
+
+let HTMLRepresentation = `<h1>The Mod Squad</h1>`
+{
+    const ModSquad = {
+        "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+        "series": {
+            "start": "1968",
+            "end": "1973"
+        }
+    }
+
+
+    ModSquad.members.forEach(member => {
+        //console.log(member)
+         HTMLRepresentation += `<div>${member}</div>`
+    })
+}
+
+document.querySelector(".show-info").innerHTML += HTMLRepresentation
